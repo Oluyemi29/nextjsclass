c0a2540a705286585cba37e3a54865f1fca2026b
Oluyemi29 <adedokunoluyemi29@gmail.com>
2024-05-24 07:55:47 +0100

    nextjs frontend class

---
+++ src/app/component/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>Sidebar</h1>
+      <h1>Sidebar</h1>
+      <h1>Sidebar</h1>
+      <h1>Sidebar</h1>
+      <h1>Sidebar</h1>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../component/Sidebar'
+
+const page = () => {
+  return (
+    <div>
+        <div>
+            <Sidebar/>
+            <h1>dashboard</h1>
+        </div>
+    </div>
+  )
+}
+
+export default page
